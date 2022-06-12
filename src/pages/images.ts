20f8ef61fce54e32afeaec2b4e7960ecbc647381
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-06-12 18:05:37 -0500

    trying out squoosh

--- src/pages/images.ts
+++ src/pages/images.ts
@@ -1,17 +1,13 @@
-import { Readable } from 'stream'
-import sharp from 'sharp'
 import { lookup } from 'mime-types'
+import { ImagePool } from '@squoosh/lib'
 import type { APIRoute } from 'astro'
 
+/*
 export const get: APIRoute = async ({ request, params }) => {
     try {
         const url = new URL(request.url)
         const src = url.searchParams.get('url').replace(/^\//, '')
-        console.log('src', src)
-        
         const href = !src.startsWith('http') ? new URL(src, url.origin) : new URL(src)
-
-        console.log('href', href.toString())
 
         const inputRes = await fetch(href.toString())
 
@@ -19,13 +15,8 @@ export const get: APIRoute = async ({ request, params }) => {
             return new Response(`"${src}" not found`, { status: 404 })
         }
 
-        console.log('got inputRes', inputRes.status, inputRes.statusText)
-
         const inputBuffer = Buffer.from(await inputRes.arrayBuffer())
-        console.log('got buffer', Buffer.byteLength(inputBuffer))
-
         const outputBuffer = await sharp(inputBuffer).resize(400).toBuffer()
-        console.log('got sharp result', Buffer.byteLength(outputBuffer))
 
         return new Response(outputBuffer, {
             headers: {
@@ -40,5 +31,59 @@ export const get: APIRoute = async ({ request, params }) => {
 
     return {
         body: `Hello, ${params.slug}`
+    }
+}
+*/
+
+export const get: APIRoute = async({ request, params }) => {
+    try {
+        const url = new URL(request.url)
+        const src = url.searchParams.get('url').replace(/^\//, '')
+        
+        const href = !src.startsWith('http') ? new URL(src, url.origin) : new URL(src)
+
+        const inputRes = await fetch(href.toString())
+
+        if (!inputRes.ok) {
+            return new Response(`"${src}" not found`, { status: 404 })
+        }
+
+        const inputBuffer = Buffer.from(await inputRes.arrayBuffer())
+
+        const pool = new ImagePool()
+        const image = pool.ingestImage(inputBuffer)
+
+        await image.decoded
+
+        await image.preprocess({
+            resize: {
+                enable: true,
+                width: 400,
+                height: 200
+            }
+        })
+
+        await image.encode({
+            mozjpeg: {},
+            jxl: {
+                quality: 90
+            }
+        })
+
+        await pool.close()
+
+        const outputBuffer = Buffer.from((await image.encodedWith.mozjpeg).binary)
+
+        return new Response(outputBuffer, {
+            headers: {
+                'Content-Length': Buffer.byteLength(outputBuffer).toString(),
+                'Content-Type': lookup(src),
+                // 'Cache-Control': 'max-age=360000'
+            }
+        })
+    } catch (err) {
+        console.error(err)
+
+        return new Response(err.toString(), { status: 500 })
     }
 }

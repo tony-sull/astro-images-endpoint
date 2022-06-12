c54503586a4706f7e5adef1e82666996da624232
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-06-12 16:58:45 -0500

    removing local globbing attempt

--- src/pages/images/index.ts
+++ src/pages/images/index.ts
@@ -1,23 +1,22 @@
-import * as npath from 'path'
 import sharp from 'sharp'
 import type { APIRoute } from 'astro'
-
-const inputs = Object.entries(import.meta.glob('./**/*')).reduce((acc, [key, value]) => {
-    acc[key.replace('./', '')] = value
-    return acc
-}, {})
 
 export const get: APIRoute = async ({ request, params }) => {
     try {
         const url = new URL(request.url)
         const src = url.searchParams.get('url').replace(/^\//, '')
-        if (!(src in inputs)) {
+        
+        const href = !src.startsWith('http') ? new URL(src, url.origin) : new URL(src)
+
+        const inputRes = await fetch(href.toString())
+
+        if (!inputRes.ok) {
             return new Response(`"${src}" not found`, { status: 404 })
         }
-        const { default: input } = await inputs[src]()
-        const inputAbs = npath.join(process.cwd(), input)
 
-        const result = await sharp(inputAbs).resize(400).jpeg({ mozjpeg: true }).toBuffer()
+        const inputBuffer = Buffer.from(await inputRes.arrayBuffer())
+
+        const result = await sharp(inputBuffer).resize(400).jpeg({ mozjpeg: true }).toBuffer()
 
         return new Response(result)
     } catch (err) {
